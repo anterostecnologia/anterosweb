3b818e50a7bcafce6ce006aa47fff478962c54c2
Edson Martins <edsonmartins2005@gmail.com>
2014-09-05 10:00:00 -0300

    Adicionado internacionalização.

---
+++ src/main/java/br/com/anteros/anteros/web/util/AnterosWebTranslate.java
@@ -0,0 +1,10 @@
+package br.com.anteros.anteros.web.util;
+
+import br.com.anteros.core.utils.AbstractCoreTranslate;
+
+public class AnterosWebTranslate extends AbstractCoreTranslate {
+
+	static {
+		bundleName = "anterossweb_messages";
+	}
+}
