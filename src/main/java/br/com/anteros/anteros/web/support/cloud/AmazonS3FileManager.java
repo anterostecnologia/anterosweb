b5c5afcf34b7371582310a863bb92bd8962294cd
Edson Martins <edsonmartins2005@gmail.com>
2019-12-01 19:01:07 -0300

    ok

--- src/main/java/br/com/anteros/anteros/web/support/cloud/AmazonS3FileManager.java
+++ src/main/java/br/com/anteros/anteros/web/support/cloud/AmazonS3FileManager.java
@@ -5,16 +5,15 @@ import br.com.anteros.persistence.session.ResultInfo;
 
 public class AmazonS3FileManager implements ExternalFileManager {
 
-	@Override
 	public ResultInfo saveFile(String folderName, String fileName, byte[] fileContent) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void removeFile(String fileName) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
+
 
 }
--- src/main/java/br/com/anteros/anteros/web/support/cloud/NextCloudFileManager.java
+++ src/main/java/br/com/anteros/anteros/web/support/cloud/NextCloudFileManager.java
@@ -59,4 +59,6 @@ public class NextCloudFileManager implements ExternalFileManager {
 
 	}
 
+	
+
 }
